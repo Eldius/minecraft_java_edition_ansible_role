d5e9b24aab9cdb22dfe1221b8e5e9d3909dd3da3
Eldio Santos <eldio.santos@m4u.com.br>
2020-02-05 20:31:54 -0300

    Some improvements...

--- templates/start_server.sh
+++ templates/start_server.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
-java -Xmx{{ minecraft_max_memory }} -Xms{{ minecraft_min_memory }} -jar {{ minecraft_install_folder }}/server.jar nogui
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseG1GC"
+JAVA_OPTS="${JAVA_OPTS} -XX:MaxRAM=512m"
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseStringDeduplication"
+JAVA_OPTS="${JAVA_OPTS} -XX:+OptimizeStringConcat"
+JAVA_OPTS="${JAVA_OPTS} -XX:+UseLWPSynchronization"
+JAVA_OPTS="${JAVA_OPTS} -Xmx{{ minecraft_max_memory }}"
+JAVA_OPTS="${JAVA_OPTS} -Xms{{ minecraft_min_memory }}"
+
+java $JAVA_OPTS -jar {{ minecraft_install_folder }}/server.jar nogui
